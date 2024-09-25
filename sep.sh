319de1de3e3799a8200cdfe4c5fdb01dea9950fa
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-09-25 13:10:57 +0300

    Create sep.sh

---
+++ sep.sh
@@ -0,0 +1,999 @@
+# A tool by nitro from www.noob-hackers.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+
+
+              
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+                     
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐──── 
+                                                  
+                                                                                              
+                                                    
+                                                    SafeMU
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[96m] استخراج  حسابات 1"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] استخراج  حسابات 2"
+echo -e "$red                        ➡$cyan [\e[92m3\e[96m] استخراج  حسابات  3 "
+echo -e "$red                        ➡$cyan [\e[92m8\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn               >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+virus
+elif [ $optnz = "2" ];
+then
+save
+elif [ $optnz = "3" ];
+then
+about
+elif [ $optnz = "4" ];
+then
+upd
+elif [ $optnz = "5" ];
+then
+am start -a android.intent.action.VIEW -d https://wa.me/967738325474 2>/dev/null 2>&1
+banner
+menu
+elif [ $optnz = "6" ];
+thenam start -a android.intent.action.VIEW -d https://wa.me/967738325474 2>/dev/null 2>&1
+banner
+menu
+elif [ $optnz = "7" ];
+then
+am start -a android.intent.action.VIEW -d https://wa.me/967738325474 2>/dev/null 2>&1
+banner
+menu
+elif [ $optnz = "8" ];
+then
+exit 1
+else
+echo "wrong"
+exit
+fi
+}
+virus(){
+################
+clear
+echo
+echo -e '\e[92m 
+               
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────      
+                    
+                    echo " "
+echo -e "\e[91m 😈   تم ي عزيزي استخراج الحسابات السيفيوم بنجاح 
+                                👇👇👇
+
+         \e[92mL1NK :- \e[96m 
+         
+         kgbtlzfhg
+
+xselhqjgx
+
+giostymxb
+
+gzonpkyck
+
+umpnvhztp
+
+ctmypdmwx
+
+dmoyfcxff
+
+vgbbmcurb
+
+notkpjmxx
+
+qhizdwhuj
+
+xuzhqqmzy
+
+ejkfcwgzs
+
+wfheoprrc
+
+vfwvtxveb
+
+lngvfrent
+
+sttdciczr
+
+svvbakazl
+
+mewgryyog
+
+rphjsdstg
+
+asljxnesw
+
+zmkbfkfkz
+
+kxtznxsjs
+
+vlsvuxgjn
+
+flazotzgf
+
+jtunakxea
+
+rohjlfyaf
+
+vooecolnm
+
+lhaozzkof
+
+zkpnsquud
+
+oyobzgdgp
+
+jdyapgcsn
+
+vcapkjoqs
+
+aoykioihz
+
+pgrxlgugy
+
+vdtxmnmby
+
+tkbxspfsw
+
+vzgoawhks
+
+aioyzggoh
+
+mgckzaovr
+
+vzfzdmjqi
+
+txrjafgxv
+
+hpwpbyonf
+
+cqxypuyou
+
+nljpaylmx
+
+myjeaaauo
+
+ckdmfmijp
+
+unhxivulu
+
+zikuapfka
+
+igjuwnkdw
+
+hdsfrbyrw
+
+ltoyymhob
+
+cswayaldp
+
+qwdcjczjm
+
+oxkbqclbm
+
+ttepkzyzn
+
+vehcxyzrb
+
+vbpajnooo
+
+btqmotnvy
+
+obdruirvr
+
+xvxjkmskf
+
+tzrnqhlam
+
+ftfxxwnwd
+
+mffhjyvmu
+
+udfkelzah
+
+ygicppmml
+
+piycubclg
+
+lulegbeda
+
+ludawexjh
+
+nqpoeyono
+
+arhfzvhcp
+
+zymtzmfzb
+
+glhtjvjfc
+
+jxgxalvoj
+
+gsxkrdrqf
+
+suungquge
+
+elzrikppp
+
+otkisxvpt
+
+alrytnbdo
+
+kbtwqmysk
+avseizaqv
+
+vwviacmam
+
+nymqeznfp
+
+kvreypdqc
+
+otgewkpak
+
+golexyzem
+
+zngdjptgi
+
+xwgcgwybi
+
+kefzcgciw
+
+pwwinejnn
+
+nibrwqoow
+
+cuhbyuldg
+
+lppplwlwt
+
+akuwychnh
+
+yijzuyeub
+
+vxtiocous
+
+czhhbdaty
+
+ddddsvkjt
+
+tveescxcn
+
+xxmidrdoz
+
+ezbfbefxn
+
+anmpdnvfu
+
+rezkkoaet
+
+indwfbdgy
+
+jqpjgwtoc
+
+riefejtkj
+
+ufunymljc
+
+eeadooumt
+
+enruhyqhn
+
+affmcbtfv
+
+dctjfnxbp
+
+fqyadmeex
+
+qjwqlonyl
+
+cspejitvb
+
+xefoawfzv
+
+wddwltwxa
+
+fgcattjjq
+
+nievpjbcg
+
+zngpyxrzz
+
+zdihelpws
+
+xfgpwwdiz
+
+dikfzyygt
+
+cscuajopd
+
+ryvatfcec
+
+xgdzluxie
+
+oyevrrtkd
+
+uskwcoqoa
+
+lommhdyid
+
+usprodebo
+
+mvsfsfmwr
+
+kxkzfgxjb
+
+ckyvwkvtv
+
+cliwaovmd
+
+qgufivqwx
+
+bflkhenia
+
+gdsztelbn
+
+ymyfgkvjm
+
+ncovyqyer
+
+xnmxgdslr
+
+saeylkvxe
+
+gyfsrmnst
+
+jeggkktqi
+
+tqwakdjez
+
+seadomzwa
+
+opzysezga
+
+swmhonxqp
+
+fjyunwose
+
+kxhrhsywq
+
+gqduhhuyq
+
+pomyuzqsu
+
+umekxecex
+
+gdoezxvpm
+
+bmbolcexf
+
+mqtvksxtk
+
+tbtpoqcvc
+
+fskkwqpuw
+
+cftknffzp
+
+wvswvtoxw
+
+wdjxiatdm
+
+pffbjqgum
+
+ttcaxicne
+
+uxshgqwjk
+
+rstpyolxn
+
+qbaqvocdx
+
+ccmquoxed
+
+bvekdwgdz
+
+ysyzodjid
+
+nyovkutxt
+
+xyfulbmfg
+
+amtfzrers
+
+fnsoohzdm
+
+xiginbzyt
+
+wruhsccyu
+
+qxzulmwkg
+
+fcaxrkovq
+
+reapfxcte
+
+dqvnwkffq
+
+eujvpamkl
+
+djncpdnic
+
+wgnanwdms
+
+حسبات سيفيوم🥱🤍:
+abjfcxngs
+
+yitlymcue
+
+xcsizaeeo
+
+kskkdlnmq
+
+lstnyvpex
+
+fcnzgxipb
+
+lxdrrcvlx
+
+axuyoncfu
+
+ikpkbtcgn
+
+yiamdgpzb
+
+rpkdvjceg
+
+jcrhnafdd
+
+kczsatjtq
+
+zolmnjqjr
+
+rlmkmyzgm
+
+pwaqzlhli
+
+sdvuwhonn
+
+wivlqivir
+
+wwpoxfsjl
+
+sxqgutfxw
+
+tsndjrbyf
+
+
+wpkojhvfa
+
+aviegxsly
+
+xfyacgvqv
+
+corgmxwsu
+
+uqdyaohjp
+
+ttompeujx
+
+dkicqfnjh
+
+dicqrfsqd
+
+vyvdjjlfy
+
+kervwjzjt
+
+tyztjqvfe
+
+visblbpux
+
+mrxpsrtxn
+
+gysuyppef
+
+knvorfzia
+
+wxrdloipm
+
+srudbwrbw
+
+dtnnavzun
+
+tvqzxzkbk
+
+oklwxotnk
+
+yodoxzaux
+
+tisfxuhup
+
+kktrbttqz
+
+swrnfddpe
+
+epjqfddjy
+
+nmlmcsknh
+
+kxunbfpbt
+
+jnbmjyghd
+
+ldgswbxnh
+
+trhvjkhky
+
+egpfepsqw
+
+xzhsiobnx
+
+nfgozctwu
+
+ocinrlqbc
+
+grkysccim
+
+dqaxwicnj
+
+trmjctvnb
+
+ktanoqpou
+
+pgrrdiutt
+
+gneuczkbp
+
+aonjsuoax
+
+hxfuikgdz
+
+gtbgbgfuc
+
+jjoscwbac
+
+oeiafmpky
+
+ewuyrfxin
+
+csigmxdfu
+
+efuttawdp
+
+rnfkjijio
+
+egnbqjifp
+
+wcvkhoryx
+
+qnbwqseei
+
+dqsfiunze
+
+kysghwrbv
+
+igajhbynz
+
+gfzeoaiwd
+
+rbyxamucv
+
+frgaxrlui
+
+zglnnzmcl
+
+nyytgdfam
+
+scvcgkxnk
+
+icvzzetab
+
+bxogyhwya
+
+sjsmvwjhg
+
+ynoemigac
+
+tftnypagl
+
+dfkitqpbp
+
+kppubdlcl
+
+ljazvssja
+
+agzzpiftx
+
+faubnbmgl
+
+ablmnkako
+
+innffrkqr
+
+eacjvhjbd
+
+zlpvjpuet
+
+brdfbxqdy
+
+rlysabpdk
+
+dgatrxtnm
+
+odxyrlynu
+
+xnxvziyyc
+
+ohocapcno
+
+ixcyjzkca
+
+ctjycgifd
+
+vdkbqyskg
+
+udrgpuhxd
+
+yadjyhzrw
+
+wnzevbjrw
+
+nahgpaifr
+
+mibiptezl
+
+pjmjteoje
+
+rjryggbza
+
+cydaiplqt
+
+ykizgvbcn
+
+igeqksvro
+
+qvzrhsdjq
+
+rvyipuwkf
+
+uqilamkeq
+
+gzubxogth
+
+jusvrksme
+
+jhqudhgzb
+
+         \e[91m
+
+                                 👆👆👆
+اقوي  اده عربية  استخراج  حسابات  سيفيوم  جاهزه  مقدمه  من  مبرمج  الاده الكنج  حمودي  الاسطوره  "
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+save(){
+clear
+echo
+echo -e '\e[92m 
+                  
+        ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────                    
+                                  
+                                  '
+echo " "
+echo " "
+echo -e "\e[93m  اقوي اده استخراج حسابات سيفيوم مقدم من الكنج حمودي الاسطوره 
+                    👇👇👇
+
+        \e[92mL1NK :- \e[96m 
+        
+        
+        vtoceqngv
+
+gprargjel
+
+wqmomlaek
+
+zrfrwmonl
+
+zqvpmwzhk
+
+ixvrhzgfl
+
+wkiiyepad
+
+rtbgqsbjh
+
+qgxtjibqn
+
+rmdcwfxml
+
+agfkiwkok
+
+ymnltgzrf
+
+nkygdczep
+
+jmdrfxuiz
+
+dybzqxddn
+
+ofpworswu
+
+szxfwmvkq
+
+clqxcmukp
+
+opmfubtxm
+
+mwpqhhgav
+
+mitiywypd
+
+kbtwqmysk
+
+mneqqbxzv
+
+qonmhfvxa
+
+ittdfiejz
+
+ybxxbrtpq
+
+rhamttxoj
+
+qtrhauufz
+
+terrpdanm
+
+rtpbxaqby
+
+tdqziiqny
+
+dppewvvsf
+
+حسبات سيفيوم🥱🤍:
+rbfgzytzy
+
+lsihykmle
+
+ehhnzkwat
+
+lmlcszflj
+
+aiojkhevk
+
+rfzsjsbsi
+
+qnjxiwzix
+
+joxdilaqy
+
+mxxdlrexv
+
+euathhmuu
+
+hyswyjymx
+
+wxmxpkewq
+
+zeeejacgi
+
+lduaabrbh
+
+oervqrpkt
+
+idgpvvemi
+
+vvocigrvu
+
+ymvrgrlut
+
+icrsksvbg
+
+uuxwuxbey
+
+jgjbmikuu
+
+osuwedrmb
+
+hvnkcijip
+
+fpcqiabrh
+
+cuyuizptk
+        
+        \e[93m
+
+                    افضل اده في مجال  الترمكس 
+                   ."
+echo
+echo -ne "\e[92m#Type exit : "
+read exitz
+if [ $exitz = "exit" ];
+then
+banner
+menu
+else
+banner
+menu
+fi
+}
+about(){
+clear
+echo -e '\e[91m
+
+         ──▐─▌──▐─▌──
+         ─▐▌─▐▌▐▌─▐▌─
+         ─█▄▀▄██▄▀▄█─
+         ──▄──██▌─▄──
+         ▄▀─█▀██▀█─▀▄
+         ▐▌▐▌─▐▌─▐▌▐▌
+         ─▐─█────█─▌─
+         ────▌──▐────
+'
+echo -e "\e[93m                  CALL ME \e[92mNITRO" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92mNOOB HACKERS" | pv -qL 10
+echo -e "\e[93m       IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
+echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
+echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
+echo -e "\e[93m              JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
+echo -e "\e[93m        MY WEBSITE:\e[92m www.noob-hackers.com\e[0m" | pv -qL 10
+echo -e "\e[92m                BYEE.............." | pv -qL 10
+sleep 6.0
+banner
+menu
+}
+upd(){
+if [ -d "$HOME/infect" ];
+then
+cd $HOME
+rm -rf infect
+elif [ -d "$HOME/Infect" ];
+then
+cd $HOME
+rm -rf Infect
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96m   الكنج  حمودي  الاسطوره  \e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/noob-hackers/infect 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m        لكنج  حمودي  الاسطوره       \e[0m"
+sleep 2.0
+cd $HOME
+cd infect
+bash infect.sh
+}
+vid(){
+FILE=$HOME/infect/noob.noob
+if [ -f "$FILE" ]; then
+pop
+else
+echo
+fi
+}
+pop(){
+clear
+echo -e "\e[96m                ╔═══════════════════════════════════╗\e[0m"
+echo -e "\e[96m                ║  \e[93mHAI, I   لكنج  حمودي  الاسطورهو\e[96m VIDEO   ║\e[0m"
+echo -e "\e[96m                ║      لكنج  حمودي  الاسطوره.....!    ║\e[0m"
+echo -e "\e[96m                ║                                   ║\e[0m"
+echo -e "\e[96m                ║        Select \e[92my\e[96m to watch\e[96m          ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║        Select \e[91mt\e[96m to cancel\e[96m         ║\e[0m"
+echo -e "\e[96m                ║                (\e[93mOR\e[96m)               \e[96m║\e[0m"
+echo -e "\e[96m                ║     Select \e[91mz\e[96m to skipforever\e[96m       ║\e[0m"
+echo -e "\e[96m                ╚═══════════════════════════════════╝\e[0m"
+echo -en "\e[32mSELECT OPTION [\e[93my/\e[93mt/\e[32m\e[93mz\e[32m]: \e[0m "
+read p
+if [ "$p" = "y" ];
+then
+am start -a android.intent.action.VIEW -d https://wa.me/967738325474 2>/dev/null
+clear
+baner1
+menu
+elif [ "$p" = "t" ];
+then
+clear
+baner1
+menu
+elif [ "$p" = "z" ];
+then
+cd $HOM/infect
+rm noob.noob
+banner
+menu
+else
+banner
+menu
+exit
+fi
+}
+banner
+menu
